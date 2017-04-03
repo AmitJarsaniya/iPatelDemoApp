0418cc6d40ee543dfc9cd2627a55f45345abf0fd
archana parmar <archana.parmar@letsnurture.com>
2017-04-03 18:53:15 +0530

    Just deployment target changed

--- iPatelDemoApp/ViewController.swift
+++ iPatelDemoApp/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()  
+        super.viewDidLoad()
+        
+        print("Just changed deployment targate")
     }
 
     override func didReceiveMemoryWarning() {
