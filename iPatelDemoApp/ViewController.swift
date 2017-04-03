05081ec01b23a83b91b4b795e9c76446e48c016b
archana parmar <archana.parmar@letsnurture.com>
2017-04-03 17:47:24 +0530

    Initial Commit

---
+++ iPatelDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPatelDemoApp
+//
+//  Created by LN-11 on 03/04/17.
+//  Copyright © 2017 LetsNurture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
